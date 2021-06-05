5084b1c95d208cc207b421da9b4f32b165112c8d
Matheus0liveira <matheusfilho98.ms@gmail.com>
2021-06-05 19:02:04 -0300

    feat: create object for errors messages

---
+++ src/schemas/Errors/messages.tsx
@@ -0,0 +1,10 @@
+export const ErrorsMessage = {
+  email: {
+    isRequired: 'O email é obrigatório',
+    isInvalid: 'Tipo de email inválido',
+  },
+  password: {
+    isRequired: 'Senha é obrigatório',
+    lessThanSix: 'No mínimo 6 caracteres',
+  },
+};
