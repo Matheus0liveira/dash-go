fffb6a02a69850915a9f1d04492819bfcab4bdc2
Matheus0liveira <matheusfilho98.ms@gmail.com>
2021-06-05 21:26:44 -0300

    feat: create fakeAPI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import SideBarDrawerProvider from 'contexts/SideBarDrawerContext';
 import { AppProps } from 'next/app';
+import { makeServer } from 'services/mirage';
 import { theme } from 'styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
